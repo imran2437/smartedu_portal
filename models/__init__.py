8f35ecfba02bcb346c0298e7d3a31c2e6b5e9987
imran2437 <alimran689@gmail.com>
2023-04-30 15:56:35 +0600

    initial commit

---
+++ models/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from ...smartedu_core.models import se_student
+from ...smartedu_core.models import se_application
+from ...smartedu_core.models import se_batch
+from ...smartedu_core.models import se_student_type
+from ...smartedu_core.models import se_education_shift
+from ...smartedu_core.models import se_semester
+from ...smartedu_core.models import se_semester_type
+from ...smartedu_core.models import se_program
+from ...smartedu_core.models import se_education_board
+from . import res_partner
